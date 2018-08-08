34b670a29f3ff79e06bf47719dea31beb3fb2ef7
sclaudon <stephane.claudon@gmail.com>
2018-08-08 10:36:19 +0200

    added canvas texts and begin zoom translation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="main">
-      <p id="title">{{ greeting }} you !</p>
       <div id="stage"></div>
     </div>
 </template>
@@ -11,9 +10,10 @@ import * as THREE from "../lib/three.js";
 module.exports = {
   data: function() {
     return {
-      greeting: "Hello",
+      stageDOMElement: null,
       scene: null,
       camera: null,
+      pageHeight: 0,
       samples: [
         {
           text: "WELCOME",
@@ -32,7 +32,7 @@ module.exports = {
           zpos: 4000
         },
         {
-          text: "PHOSPHORESCENT NEEDLS & NUMBERS",
+          text: "PHOSPHORESCENT NEEDLES & NUMBERS",
           zpos: 5000
         }
       ]
@@ -41,64 +41,104 @@ module.exports = {
   mounted: function() {
     this.initScene();
     this.addContentInSpace();
+
+    document.body.style.height = this.pageHeight + "px";
   },
 
   methods: {
     initScene: function() {
-      let scene = new THREE.Scene();
-      let camera = new THREE.PerspectiveCamera(
+      this.stageDOMElement = document.getElementById("stage");
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(
         45,
-        window.innerWidth / window.innerHeight,
+        this.stageDOMElement.clientWidth / this.stageDOMElement.clientHeight,
         1,
         2e3
       );
 
       let renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setSize(
+        this.stageDOMElement.clientWidth,
+        this.stageDOMElement.clientHeight
+      );
       renderer.autoClear = !1;
       renderer.setClearColor(0, 0);
       renderer.setPixelRatio(
         window.devicePixelRatio || window.webkitDevicePixelRatio || 1
       );
 
-      document.getElementById("stage").appendChild(renderer.domElement);
+      this.stageDOMElement.appendChild(renderer.domElement);
+      this.camera.position.set(0, 0, 500);
 
-      let geometry = new THREE.BoxGeometry(1, 1, 1);
-      let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      let cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-
-      camera.position.set(0, 0, 5);
-
-      this.scene = scene;
-      this.camera = camera;
-
-      console.log(this.scene);
-
-      let animate = function() {
+      let animate = () => {
         requestAnimationFrame(animate);
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
-        renderer.render(scene, camera);
+        this.camera.position.set(
+          0,
+          0,
+          -(document.scrollingElement || document.documentElement).scrollTop
+        );
+        renderer.render(this.scene, this.camera);
       };
-
+      document.addEventListener("mousemove", (e) => {
+        console.log(e.clientX, e.clientY);
+    
+        var t = new THREE.Vector2(e.clientX, e.clientY);
+        let a = new THREE.Quaternion();
+        this.camera.rotation = new THREE.Euler(-t.y, -t.x, 0);
+      });
       animate();
     },
 
     addContentInSpace: function() {
-      /*for (let index = 0; index < this.samples.length; index++) {
-        i.fillText(e.best.rank, 12 - c.width / 2, s + 8)
-        geometry.position.z = -this.samples[index].zpos;
-        this.scene.add(geometry);
-      }*/
+      for (let index = 0; index < this.samples.length; index++) {
+        let texture = new THREE.Texture(
+          this.createCanvasText(this.samples[index].text)
+        );
+        texture.needsUpdate = true;
+        let material = new THREE.MeshBasicMaterial({ map: texture });
+        let geometry = new THREE.PlaneGeometry(200, 200);
+        let mesh = new THREE.Mesh(geometry, material);
+        mesh.position.z = -this.samples[index].zpos;
+        this.pageHeight = this.samples[index].zpos;
+        this.scene.add(mesh);
+      }
+    },
+
+    createCanvasText: function(text) {
+      let canvas = document.createElement("canvas");
+      //document.body.appendChild(canvas);
+      canvas.width = THREE.Math.ceilPowerOfTwo(
+        this.stageDOMElement.clientWidth
+      );
+      canvas.height = THREE.Math.ceilPowerOfTwo(
+        this.stageDOMElement.clientHeight
+      );
+
+      let ctx = canvas.getContext("2d");
+      ctx.font = "20pt Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+      console.log(text);
+
+      return canvas;
     }
+  },
+
+  onMouseMoved: function(e) {
+    
+  },
+
+  unknownFunc: function(e) {
+    var t = h >> 1,
+      n = m >> 1;
+    return (e.x = (e.x - t) / t), (e.y = (e.y - n) / n), e;
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @font-face {
   font-family: Roboto-Light;
   src: url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Light.eot);
@@ -132,12 +172,29 @@ module.exports = {
   src: url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.eot);
   src: url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.eot?#iefix)
       format("embedded-opentype"),
-    url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.woff) format("woff"),
+    url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.woff)
+      format("woff"),
     url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.ttf)
       format("truetype"),
     url(https://www.ulysse-nardin.com/sites/all/themes/un_subtheme/css/fonts/Roboto-Bold.svg#Roboto-Bold)
       format("svg");
   font-style: normal;
   font-weight: normal;
+}
+
+#main {
+  width: 100vw;
+  height: 100vh;
+  #stage {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    canvas {
+      z-index: 0;
+      position: fixed;
+      top: 0;
+      left: 0;
+    }
+  }
 }
 </style>

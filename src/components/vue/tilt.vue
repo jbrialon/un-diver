47f60f3059e8d742735d2a4adb2f71989db01e0b
Jérém <jeremzor@gmail.com>
2018-08-30 12:59:39 +0200

    tilt component

---
+++ src/components/vue/tilt.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tilt">
+    <img ref="image" src="../../assets/parralax.jpg">
+  </div>
+</template>
+
+<script>
+import { TweenMax, Power1 } from 'gsap'
+
+export default {
+  name: 'tilt',
+  methods: {
+    tilt (event) {
+      let posX = event.pageX
+      let posY = event.pageY
+      let left = window.innerWidth / 2 - posX
+      let top = window.innerHeight / 2 - posY
+      TweenMax.to(this.$refs.image, 0.6, {
+        rotationY: left / 100,
+        rotationX: top / 100,
+        ease: Power1.easeOut,
+        transformPerspective: 400,
+        transformOrigin: 'center'
+      })
+    }
+  },
+  mounted () {
+    document.addEventListener('mousemove', this.tilt)
+  },
+  beforeDestroy () {
+    document.removeEventListener('mousemove', this.tilt)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/_vars.scss';
+
+.tilt {
+  position:absolute;
+  z-index:10;
+  top:20%;
+  left:20%;
+}
+</style>
